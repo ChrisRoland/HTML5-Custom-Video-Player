eb21a32425d9f3bd60a58312305672155acf7a3c
ChrisRoland <chrisebuberoland@gmail.com>
2025-03-02 14:51:34 +0100

    complete

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = player.querySelector('.fullscreen');
 
 function togglePlay() {
     if (video.paused) {
@@ -22,9 +23,10 @@ function togglePlay() {
 }
 
 video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-toggle.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
 
 function updateButton() {
 
@@ -47,3 +49,38 @@ function skip() {
 }
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+      player.requestFullscreen().catch(err => {
+        console.error(`Error attempting to enable fullscreen: ${err.message}`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+}
+  
+fullscreenButton.addEventListener('click', toggleFullscreen);
